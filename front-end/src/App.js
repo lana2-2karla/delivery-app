b7f0a38287b51e18be63a25221f89ab3738ba565
Allana Karla <allanakarla1702@gmail.com>
2022-08-25 18:54:45 -0300

    :recycle: refactor: set BrowserRouter

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Login /> } />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
