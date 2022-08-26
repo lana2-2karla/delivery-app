436fe479d823334d8cad8cfe0a2aebc85d040ead
Allana Karla <allanakarla1702@gmail.com>
2022-08-26 16:26:16 -0300

    :fire: refactor: remove BrowserRouter

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <Login /> } />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+    </Routes>
   );
 }
 
