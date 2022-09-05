60170a00554571ee6d95dd211e6713de9c792831
Allana Karla <allanakarla1702@gmail.com>
2022-09-05 19:43:12 -0300

    :sparkles: feat: create Route /customer/orders/:id

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Products from './pages/customer/Products';
 import Orders from './pages/customer/Orders';
+import OrderDetails from './pages/customer/OrderDetails';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <Products /> } />
       <Route path="/customer/orders" element={ <Orders /> } />
+      <Route path="/customer/orders/:id" element={ <OrderDetails /> } />
     </Routes>
   );
 }
